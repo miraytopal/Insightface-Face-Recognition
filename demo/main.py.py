acd4d375a316f7b4990965e05d3506bd6797a5d9
Miray TOPAL <miraytopal92@gmail.com>
2023-05-17 21:04:52 +0300

    Add files via upload

---
+++ demo/main.py.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import numpy as np
+from PIL import Image
+import insightface
+from model import process_images
+from insightface.app import FaceAnalysis
+
+# user will import a picture file
+# We will show this picture to the user
+# A button will be shown to the user to start prediction
+# After prediction, the result will be shown to the user
+
+
+@st.cache_resource
+def detector_model():
+    detector = insightface.model_zoo.get_model('model.onnx', download=True)
+    return detector
+
+@st.cache_resource
+def app_model():
+    app = FaceAnalysis()
+    app.prepare(ctx_id=-1)
+    return app
+
+
+st.title("Face Recognition App")
+
+col1, col2 = st.columns(2)
+
+with col1:
+
+    file_1 = st.file_uploader("Please upload an image file", type=["jpg", "png", "jpeg"],key='file_1')
+
+    if file_1:
+        st.image(file_1)
+
+with col2:
+
+    file_2 = st.file_uploader("Please upload an image file", type=["jpg", "png", "jpeg"],key='file_2')
+
+    if file_2:
+        st.image(file_2)
+
+
+if st.button("Predict"):
+
+    if not (file_1 and file_2):
+        st.error("Please upload two image files")
+
+    else:
+        # File to numpy array
+        image_1 = np.asarray(Image.open(file_1))
+        image_2 = np.asarray(Image.open(file_2))
+
+        score = process_images(image_1, image_2, detector_model(), app_model())
+
+        st.metric("Similarity", f"{score:.2f}")
+
+
+
---
+++ demo/model.py.py
@@ -0,0 +1,26 @@
+
+def process_images(img1, img2, detector, app):
+
+  """
+  Calculate the similarity score between the two images.
+
+  Args:
+    image1_path : str, path of first image
+    image1_path : str, path of second image
+
+  Returns:
+    float, similarity score
+
+  """
+
+
+  # to prepare images
+
+  faces_1 = app.get(img1)[0]['embedding']
+  faces_2 = app.get(img2)[0]['embedding']
+
+
+  score =  detector.compute_sim(faces_1, faces_2)
+  # to compare image similarity
+  print(f'Similarity score: {score}')
+  return score
