594bc8b4d53d6b28cc37c4147d06c2afb12adc3b
Miray TOPAL <miraytopal92@gmail.com>
2023-05-17 21:06:22 +0300

    Update model.py.py

--- demo/model.py.py
+++ demo/model.py.py
@@ -13,12 +13,10 @@ def process_images(img1, img2, detector, app):
 
   """
 
-
   # to prepare images
 
   faces_1 = app.get(img1)[0]['embedding']
   faces_2 = app.get(img2)[0]['embedding']
-
 
   score =  detector.compute_sim(faces_1, faces_2)
   # to compare image similarity
